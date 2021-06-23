2e2889c87b0734764456db57fb6429d153c930c0
Nasib Mammadov <bduti09@gmail.com>
2021-06-24 00:51:21 +0400

    new project video-chat

---
+++ video-chat/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navigation/>
+    <router-view />
+  </div>
+</template>
+<script>
+import Navigation from "@/components/Navigation";
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Navigation
+  }
+};
+</script>
+<style lang="scss">
+$primary: #5f2882;
+//import bootstrap
+@import "node_modules/bootstrap/scss/bootstrap";
+</style>
---
+++ video-chat/src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/login">Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation.vue"
+};
+</script>
+
+<style scoped>
+</style>
